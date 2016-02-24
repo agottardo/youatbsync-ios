cb80d148140abb640a9590959a7dda16d10e0a2a
Andrea Gottardo <andrea@gottardo.me>
2016-02-24 23:00:16 +0100

    Initial Commit

---
+++ BocconiSync/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BocconiSync
+//
+//  Created by Andrea Gottardo on 2/24/16.
+//  Copyright © 2016 Andrea Gottardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BocconiSync/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BocconiSync
+//
+//  Created by Andrea Gottardo on 2/24/16.
+//  Copyright © 2016 Andrea Gottardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
